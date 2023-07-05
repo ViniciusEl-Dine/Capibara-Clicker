bbe60fc870f6f12d5ada2716d35130bfb7bbd019
Vinicius Tomacheusk El-Dine <101994742+ViniciusEl-Dine@users.noreply.github.com>
2023-07-05 14:34:53 -0300

    criando animação de click na capibara

--- capibara clicker/script.js
+++ capibara clicker/script.js
@@ -1,5 +1,21 @@
-function cursor(){
-    document.querySelector('.capibara').addEventListener('mouseover',() => {
-        document.querySelector('.element').style.cursor = 'pointer';
-    });
-}
+const capibara = document.querySelector(".capibaraIMG");
+
+const createParticle = (x,y) =>{
+    const capibaraCLicks = document.querySelector(".capibara");
+
+    const particle = document.createElement("img");
+    particle.setAttribute("src","./img/s2.png");
+    particle.setAttribute("class","capybara-particle");
+    particle.style.left = x + "px";
+    particle.style.top = y + "px";
+
+    capibaraCLicks.appendChild(particle);
+
+    setTimeout(() => {
+        capibaraCLicks.removeChild(particle);
+    }, 1000);
+};
+
+capibara.addEventListener("click", (e) =>{
+    createParticle(e.clientX,e.clientY)
+});
